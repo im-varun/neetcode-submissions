36d035943abcda3435a5f78752935a583fd37585
Varun Mulchandani <143339954+im-varun@users.noreply.github.com>
2026-09-10 18:24:29 -0500

    Add: count-odd-numbers-in-an-interval-range - submission-2

---
+++ Data Structures & Algorithms/count-odd-numbers-in-an-interval-range/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countOdds(self, low: int, high: int) -> int:
+        length = high - low + 1
+
+        base_count = length // 2
+        count = base_count
+
+        if length % 2 == 1 and low % 2 == 1:
+            count += 1
+        
+        return count
